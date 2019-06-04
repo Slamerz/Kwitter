56b226f218edd05844ff3222d5d8e7fc21aa06a3
Unknown <keybladeschoosen1@gmail.com>
2019-06-04 10:55:19 -0400

    Updated scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import { LoginForm, UserProfile } from ".";
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" render={() => <LoginForm />} />
-        <Route exact path="/profile" render={() => <UserProfile />} />
-      </Switch>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" render={() => <LoginForm />} />
+    <Route exact path="/profile" render={() => <UserProfile />} />
+  </Switch>
+);
 
 export default App;
