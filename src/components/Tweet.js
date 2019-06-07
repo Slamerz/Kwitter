ea9b74a3004c63971c4f010c9596926f9c4c4f9e
Unknown <keybladeschoosen1@gmail.com>
2019-06-07 11:30:21 -0400

    Added css stying

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,12 +4,14 @@ import LikeButton from "./LikeButton";
 class Tweet extends Component {
   render() {
     return (
-      <div key={this.props.key}>
+      <div key={this.props.key} className="tweet">
         <img
-          src="https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png"/>
-        <span>{this.props.author.displayName}</span>
-        <span>{this.props.author.username}</span>
-        <span>{this.props.message.createdAt}</span>
+          src="https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png"
+          className="avatar"
+        />
+        <span className="displayName">{this.props.author.displayName}</span>
+        <span className="username">{this.props.author.username}</span>
+        <span className="date">{this.props.message.createdAt}</span>
         <p>{this.props.message.text}</p>
         <LikeButton>{this.props.message.likes.length}</LikeButton>
       </div>
