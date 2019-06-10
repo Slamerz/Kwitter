b0a7ef967f2cb978f3a4dfd9bad129734dbdeecd
Unknown <keybladeschoosen1@gmail.com>
2019-06-10 10:21:30 -0400

    Added prettier

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 import Username from "./Username";
 
 class UserList extends Component {
-    render() {
-        const users = this.props.users.map((user, key) => <Username user={user} key={key}/>);
-        return (
-            <div className="userlist">
-                {users}
-            </div>
-        );
-    }
+  render() {
+    const users = this.props.users.map((user, key) => (
+      <Username user={user} key={key} />
+    ));
+    return <div className="userlist">{users}</div>;
+  }
 }
 export default UserList;
