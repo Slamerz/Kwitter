ddd418927a200e96cc3204be52edd6853df3573e
Unknown <keybladeschoosen1@gmail.com>
2019-06-10 13:26:15 -0400

    added VisibleFeed Container

---
+++ src/containers/VisibleFeed.js
@@ -0,0 +1,41 @@
+import { connect } from "react-redux";
+import Feed from "../components/Feed";
+import React from "react";
+import { fetchTweets, fetchUsers } from "../actions";
+
+class VisibleFeed extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchTweets());
+    this.props.dispatch(fetchUsers());
+  }
+
+  render() {
+    const {
+      tweetsError,
+      tweetsLoading,
+      tweets,
+      users,
+      usersLoading,
+      usersError
+    } = this.props;
+    if (usersError || tweetsError) {
+      return <div> Error! {users.message}</div>;
+    }
+    if (usersLoading || tweetsLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return <Feed tweets={tweets} users={users} />;
+  }
+}
+
+const mapStateToProps = state => ({
+  users: state.users.users,
+  usersLoading: state.users.loading,
+  usersError: state.users.error,
+  tweets: state.messages.tweets,
+  tweetsLoading: state.messages.loading,
+  tweetsError: state.messages.error
+});
+
+export default connect(mapStateToProps)(VisibleFeed);
