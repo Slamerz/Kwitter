8a9c2174d492bfb90e12545b72b20c408280de61
Unknown <keybladeschoosen1@gmail.com>
2019-06-13 13:46:34 -0400

    improved style and added link

--- src/components/Username.js
+++ src/components/Username.js
@@ -3,12 +3,10 @@ import { Button } from "shards-react";
 import { Nav, NavItem, NavLink } from "shards-react";
 
 const Username = props => (
-  <Button pill outline theme="info">
+  <Button pill outline theme="info" className="usernamebutton">
     <Nav>
       <NavItem>
-        <NavLink to={"/users/" + props.user.id} active={true}>
-          @{props.user.username}
-        </NavLink>
+        <NavLink>@{props.user.username}</NavLink>
       </NavItem>
     </Nav>
   </Button>
