7a8115535813337f35856c796207bba2e692f282
Unknown <keybladeschoosen1@gmail.com>
2019-06-13 12:18:38 -0400

    updated router for params and landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { LoginForm, UserProfile,Homepage  } from ".";
-
+import { LoginForm, UserProfile, Homepage } from ".";
 
 const App = () => (
   <Switch>
-    <Route exact path="/" render={() => <LoginForm />} />
-    <Route exact path="/profile" render={() => <UserProfile />} />
-    <Route exact path="/homepage" render={() => <Homepage />} />
+    <Route exact path="/" component={LoginForm} />
+    <Route exact path="/profile/:id" component={UserProfile} />
+    <Route exact path="/homepage" component={Homepage} />
   </Switch>
 );
 
