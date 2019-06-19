42d375783c7cda898bd94d1e2964997c465e2905
Unknown <keybladeschoosen1@gmail.com>
2019-06-19 11:55:15 -0400

    update LikeButton.js
    added post and delete actions, and checks if post is liked

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { Button} from "shards-react";
+import { Button } from "shards-react";
 
 class LikeButton extends React.Component {
-
   render() {
-      const {messageId, likes, login, postLike} = this.props;
-      this.doesLike = likes.find(like => like.userId === login.id);
+    const { messageId, likes, login, likeActions } = this.props;
+    this.doesLike = likes.find(like => like.userId === login.id);
     return (
-        <div>
-            <a onClick={() => postLike(messageId, login.token)}><Button active={this.doesLike} pill outline theme="info">{likes.length}</Button></a>
-        </div>
+      <div>
+        <a
+          onClick={() =>
+            this.doesLike
+              ? likeActions.delete(this.doesLike.id, login.token)
+              : likeActions.post(messageId, login.token)
+          }
+        >
+          <Button active={this.doesLike} pill outline theme="info">
+            {likes.length}
+          </Button>
+        </a>
+      </div>
     );
   }
 }
